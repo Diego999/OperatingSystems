a87a32ff8f044a196a60ac851adf4908a17e17fc
Jason Racine <jason.racine@epfl.ch>
2015-02-23 22:54:26 +0100

    Corrected bugs in Lab1 -> now everything's ok !

--- Labs1/get_unique_id.c
+++ Labs1/get_unique_id.c
@@ -4,14 +4,17 @@
 
 asmlinkage long sys_get_unique_id(int *uuid)
 {
+	// initialize a static atomic counter
 	static atomic_t id = ATOMIC_INIT(0);
 
-	//Check whether the address is in the user-space memory
-	if(put_user(0, uuid) != 0)
+	// check wether the address is in the user-space memory
+	if (put_user(0, uuid) != 0)
 		return -EFAULT;
 
+	// increment the counter
 	atomic_inc(&id);
 	put_user(atomic_read(&id), uuid);
 
+	// final return
 	return 0;
 }
