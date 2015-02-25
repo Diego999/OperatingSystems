fe7859a5f5827379d103295f636a3afc85d9ad8b
Jason Racine <jason.racine@epfl.ch>
2015-02-25 11:02:38 +0100

    Replaced atomic by lock for get_unique_id

--- Labs1/get_unique_id.c
+++ Labs1/get_unique_id.c
@@ -1,19 +1,28 @@
 #include <linux/linkage.h>
 #include <linux/uaccess.h>
-#include <linux/atomic.h>
+#include <linux/semaphore.h>
 
 asmlinkage long sys_get_unique_id(int *uuid)
 {
-	// initialize a static atomic counter
-	static atomic_t id = ATOMIC_INIT(0);
+	// initialize a static counter with lock
+	static DEFINE_SPINLOCK(counter_lock);
+	static unsigned int counter = 0;
+
+	// initialize kernel-space var for gathering uuid
+	unsigned int obtained_uuid = 0;
 
 	// check wether the address is in the user-space memory
 	if (put_user(0, uuid) != 0)
 		return -EFAULT;
 
-	// increment the counter
-	atomic_inc(&id);
-	put_user(atomic_read(&id), uuid);
+	// increment the counter and get back the result in kernel space (no interrupt)
+	spin_lock(&counter_lock);
+	counter++;
+	obtained_uuid = counter;
+	spin_unlock(&counter_lock);
+
+	// put result in user space, interruptable
+	put_user(obtained_uuid, uuid);
 
 	// final return
 	return 0;
